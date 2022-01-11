41cf97d399352dbbcc532896d65527fbffb0d0f9
Mtestor <mucevval@gmail.com>
2022-01-11 13:35:46 +0100

    Added unit tests for PawnMovePredicatImplementation

--- UnitTest.py
+++ UnitTest.py
@@ -1,4 +1,5 @@
 from Pawn import Pawn, PawnColor, PawnType
+import PawnMovePredicatImplementation as pwmpi
 import Movement
 import GameMap as gm
 
@@ -28,8 +29,100 @@ def is_pos_with_pawn():
     gm.gameMap[p] = pawn
     assert(isWithoutPawn == False and "They should have a pawn at position p")
 
+def is_horizontal_pawn_movement_predicat_to_north_correct():
+    pBeg = (2, 2)
+    pEnd = (1, 2)
+    gm.gameMap[pBeg] = Pawn(PawnColor.NONE, PawnType.HORIZONTAL)
+    gm.gameMap[pEnd] = None
+    isMovableTo = pwmpi.is_horizontal_pawn_deplacement(pBeg, pEnd)
+    assert(isMovableTo == True and "The horizontal pawn should be able move to one case north")
+
+def is_horizontal_pawn_movement_predicat_to_south_correct():
+    pBeg = (2, 2)
+    pEnd = (3, 2)
+    gm.gameMap[pBeg] = Pawn(PawnColor.NONE, PawnType.HORIZONTAL)
+    gm.gameMap[pEnd] = None
+    isMovableTo = pwmpi.is_horizontal_pawn_deplacement(pBeg, pEnd)
+    assert(isMovableTo == True and "The horizontal pawn should be able move to one case south")
+
+def is_horizontal_pawn_movement_predicat_to_east_correct():
+    pBeg = (2, 2)
+    pEnd = (2, 3)
+    gm.gameMap[pBeg] = Pawn(PawnColor.NONE, PawnType.HORIZONTAL)
+    gm.gameMap[pEnd] = None
+    isMovableTo = pwmpi.is_horizontal_pawn_deplacement(pBeg, pEnd)
+    assert(isMovableTo == True and "The horizontal pawn should be able move to one case east")
+
+def is_horizontal_pawn_movement_predicat_to_west_correct():
+    pBeg = (2, 2)
+    pEnd = (2, 1)
+    gm.gameMap[pBeg] = Pawn(PawnColor.NONE, PawnType.HORIZONTAL)
+    gm.gameMap[pEnd] = None
+    isMovableTo = pwmpi.is_horizontal_pawn_deplacement(pBeg, pEnd)
+    assert(isMovableTo == True and "The horizontal pawn should be able move to one case west")
+
+def is_horizontal_pawn_movement_predicat_to_wrong_pos_correct():
+    pBeg = (2, 2)
+    pEnd = (1, 1)
+    gm.gameMap[pBeg] = Pawn(PawnColor.NONE, PawnType.HORIZONTAL)
+    gm.gameMap[pEnd] = None
+    isMovableTo = pwmpi.is_horizontal_pawn_deplacement(pBeg, pEnd)
+    assert(isMovableTo == False and "The horizontal pawn shouldn't be able move to one case north-west")
+
+def is_diagonal_pawn_movement_predicat_to_southwest_correct():
+    pBeg = (2, 2)
+    pEnd = (3, 1)
+    gm.gameMap[pBeg] = Pawn(PawnColor.NONE, PawnType.HORIZONTAL)
+    gm.gameMap[pEnd] = None
+    isMovableTo = pwmpi.is_diagonal_pawn_deplacement(pBeg, pEnd)
+    assert(isMovableTo == True and "The diagonal pawn should be able move to one case south-west")
+
+def is_diagonal_pawn_movement_predicat_to_northwest_correct():
+    pBeg = (2, 2)
+    pEnd = (1, 1)
+    gm.gameMap[pBeg] = Pawn(PawnColor.NONE, PawnType.HORIZONTAL)
+    gm.gameMap[pEnd] = None
+    isMovableTo = pwmpi.is_diagonal_pawn_deplacement(pBeg, pEnd)
+    assert(isMovableTo == True and "The diagonal pawn should be able move to one case north-west")
+
+def is_diagonal_pawn_movement_predicat_to_northeast_correct():
+    pBeg = (2, 2)
+    pEnd = (1, 3)
+    gm.gameMap[pBeg] = Pawn(PawnColor.NONE, PawnType.HORIZONTAL)
+    gm.gameMap[pEnd] = None
+    isMovableTo = pwmpi.is_diagonal_pawn_deplacement(pBeg, pEnd)
+    assert(isMovableTo == True and "The diagonal pawn should be able move to one case north-east")
+
+def is_diagonal_pawn_movement_predicat_to_southeast_correct():
+    pBeg = (2, 2)
+    pEnd = (3, 3)
+    gm.gameMap[pBeg] = Pawn(PawnColor.NONE, PawnType.HORIZONTAL)
+    gm.gameMap[pEnd] = None
+    isMovableTo = pwmpi.is_diagonal_pawn_deplacement(pBeg, pEnd)
+    assert(isMovableTo == True and "The diagonal pawn should be able move to one case south-east")
+
+def is_diagonal_pawn_movement_predicat_to_wrong_pos_correct():
+    pBeg = (2, 2)
+    pEnd = (2, 1)
+    gm.gameMap[pBeg] = Pawn(PawnColor.NONE, PawnType.HORIZONTAL)
+    gm.gameMap[pEnd] = None
+    isMovableTo = pwmpi.is_diagonal_pawn_deplacement(pBeg, pEnd)
+    assert(isMovableTo == False and "The diagonal pawn shouldn't be able move to one case west")
 
 is_pos_inside_border()
 is_pos_outside_border()
+
 is_pos_without_pawn()
 is_pos_with_pawn()
+
+is_horizontal_pawn_movement_predicat_to_north_correct()
+is_horizontal_pawn_movement_predicat_to_south_correct()
+is_horizontal_pawn_movement_predicat_to_east_correct()
+is_horizontal_pawn_movement_predicat_to_west_correct()
+is_horizontal_pawn_movement_predicat_to_wrong_pos_correct()
+
+is_diagonal_pawn_movement_predicat_to_southwest_correct()
+is_diagonal_pawn_movement_predicat_to_northwest_correct()
+is_diagonal_pawn_movement_predicat_to_northeast_correct()
+is_diagonal_pawn_movement_predicat_to_southeast_correct()
+is_diagonal_pawn_movement_predicat_to_wrong_pos_correct()
