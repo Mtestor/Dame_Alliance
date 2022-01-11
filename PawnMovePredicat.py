b35f02d5c8bf3dde5bd6b48b3f32b51cd67947b5
Mtestor <mucevval@gmail.com>
2022-01-11 13:15:47 +0100

    Moved Implementation to their own files

--- PawnMovePredicat.py
+++ PawnMovePredicat.py
@@ -1,51 +1,10 @@
 import Pawn as pw
+import PawnMovePredicatImplementation as pwmpi
 
 pawnMovePredicat = dict()
 for k in pw.PawnType:
     pawnMovePredicat[k] = list()
 
-pawnMovePredicat[pw.PawnType.HORIZONTAL].append()
-
-def is_horizontal_pawn_deplacement(posBegin : tuple, posEnd : tuple):
-    # pos[0] is row and pos[1] is colomn because of gm.gameMap
-    if posBegin[0] == posEnd[0] and posBegin[1] + 1 == posEnd[1]:
-        return True
-    if posBegin[0] == posEnd[0] and posBegin[1] - 1 == posEnd[1]:
-        return True
-    if posBegin[0] + 1 == posEnd[0] and posBegin[1] == posEnd[1]:
-        return True
-    if posBegin[0] - 1 == posEnd[0] and posBegin[1] == posEnd[1]:
-        return True
-    return False
-
-def is_diagonal_pawn_deplacement(posBegin : tuple, posEnd : tuple):
-    # pos[0] is row and pos[1] is colomn because of gm.gameMap
-    if posBegin[0] + 1 == posEnd[0] and posBegin[1] + 1 == posEnd[1]:
-        return True
-    if posBegin[0] - 1 == posEnd[0] and posBegin[1] - 1 == posEnd[1]:
-        return True
-    if posBegin[0] + 1 == posEnd[0] and posBegin[1] - 1 == posEnd[1]:
-        return True
-    if posBegin[0] - 1 == posEnd[0] and posBegin[1] + 1 == posEnd[1]:
-        return True
-    return False
-
-def is_royal_pawn_deplacement(posBegin : tuple, posEnd : tuple):
-    # pos[0] is row and pos[1] is colomn because of gm.gameMap
-    if posBegin[0] == posEnd[0] and posBegin[1] + 1 == posEnd[1]:
-        return True
-    if posBegin[0] == posEnd[0] and posBegin[1] - 1 == posEnd[1]:
-        return True
-    if posBegin[0] + 1 == posEnd[0] and posBegin[1] == posEnd[1]:
-        return True
-    if posBegin[0] - 1 == posEnd[0] and posBegin[1] == posEnd[1]:
-        return True
-    if posBegin[0] + 1 == posEnd[0] and posBegin[1] + 1 == posEnd[1]:
-        return True
-    if posBegin[0] - 1 == posEnd[0] and posBegin[1] - 1 == posEnd[1]:
-        return True
-    if posBegin[0] + 1 == posEnd[0] and posBegin[1] - 1 == posEnd[1]:
-        return True
-    if posBegin[0] - 1 == posEnd[0] and posBegin[1] + 1 == posEnd[1]:
-        return True
-    return False
+pawnMovePredicat[pw.PawnType.HORIZONTAL].append(pwmpi.is_horizontal_pawn_deplacement)
+pawnMovePredicat[pw.PawnType.DIAGONAL].append(pwmpi.is_diagonal_pawn_deplacement)
+pawnMovePredicat[pw.PawnType.ROYAL].append(pwmpi.is_royal_pawn_deplacement)
