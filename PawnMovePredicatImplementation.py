b8a684b0e6926b1a29f5f08617906cc7c13dadf9
Mtestor <mucevval@gmail.com>
2022-01-11 13:25:39 +0100

    Make a more clever is_royal_pawn_deplacement() function

--- PawnMovePredicatImplementation.py
+++ PawnMovePredicatImplementation.py
@@ -25,20 +25,8 @@ def is_diagonal_pawn_deplacement(posBegin : tuple, posEnd : tuple):
 
 def is_royal_pawn_deplacement(posBegin : tuple, posEnd : tuple):
     # pos[0] is row and pos[1] is colomn because of gm.gameMap
-    if posBegin[0] == posEnd[0] and posBegin[1] + 1 == posEnd[1]:
+    if is_horizontal_pawn_deplacement(posBegin, posEnd):
         return True
-    if posBegin[0] == posEnd[0] and posBegin[1] - 1 == posEnd[1]:
-        return True
-    if posBegin[0] + 1 == posEnd[0] and posBegin[1] == posEnd[1]:
-        return True
-    if posBegin[0] - 1 == posEnd[0] and posBegin[1] == posEnd[1]:
-        return True
-    if posBegin[0] + 1 == posEnd[0] and posBegin[1] + 1 == posEnd[1]:
-        return True
-    if posBegin[0] - 1 == posEnd[0] and posBegin[1] - 1 == posEnd[1]:
-        return True
-    if posBegin[0] + 1 == posEnd[0] and posBegin[1] - 1 == posEnd[1]:
-        return True
-    if posBegin[0] - 1 == posEnd[0] and posBegin[1] + 1 == posEnd[1]:
+    if is_diagonal_pawn_deplacement(posBegin, posEnd):
         return True
     return False
