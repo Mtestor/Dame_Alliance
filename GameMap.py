7aebfedb05b816f9de8c78b7f731ee0c88edcad7
Mtestor <mucevval@gmail.com>
2022-01-11 18:28:15 +0100

    Modified the initialization of gameMap in a way that prevent multiple reference to one object

--- GameMap.py
+++ GameMap.py
@@ -7,8 +7,9 @@ COLOMN_MAX = 7
 gameMap = full((ROW_MAX + 1, COLOMN_MAX + 1), None)
 
 def init():
-    gameMap[1,1:7] = Pawn(PawnColor.BLACK, PawnType.HORIZONTAL)
-    gameMap[2,1:7] = Pawn(PawnColor.BLACK, PawnType.DIAGONAL)
+    for k in range(1, 7):
+        gameMap[1, k] = Pawn(PawnColor.BLACK, PawnType.HORIZONTAL)
+        gameMap[2, k] = Pawn(PawnColor.BLACK, PawnType.DIAGONAL)
 
-    gameMap[5,1:7] = Pawn(PawnColor.WHITE, PawnType.DIAGONAL)
-    gameMap[6,1:7] = Pawn(PawnColor.WHITE, PawnType.HORIZONTAL)
+        gameMap[5, k] = Pawn(PawnColor.WHITE, PawnType.DIAGONAL)
+        gameMap[6, k] = Pawn(PawnColor.WHITE, PawnType.HORIZONTAL)
