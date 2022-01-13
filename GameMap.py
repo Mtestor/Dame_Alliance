adb14aeac9b3c1f2f9531ff9b66b66d0f0f27e9a
Mtestor <mucevval@gmail.com>
2022-01-13 12:55:43 +0100

    Added a function that produce GameMapState from jsom and one that initialize gameMap

--- GameMap.py
+++ GameMap.py
@@ -1,5 +1,5 @@
 from numpy import RankWarning, full
-from Pawn import *
+import Pawn as pw
 
 ROW_MAX = 7
 COLOMN_MAX = 7
@@ -17,44 +17,53 @@ def gameMap_to_json():
                 jsoned[r].append(gameMap[r,c].to_json())
     return jsoned
 
+def gameMap_from_json(jsoned):
+    for r in range(ROW_MAX + 1):
+        for c in range(COLOMN_MAX + 1):
+            if jsoned[r][c] == None:
+                gameMap[r,c] = None
+            else:
+                gameMap[r,c] = pw.pawn_from_json(jsoned[r][c])
+
 class GameMapState:
     """Hold the state of the gameMap"""
-    def __init__(self):
-        self.m_BlackNormalPawn = 12
-        self.m_BlackRoyalPawn = 0
-        self.m_WhiteNormalPawn = 12
-        self.m_WhiteRoyalPawn = 0
-        self.m_BlackScore = 0
-        self.m_WhiteScore = 0
+    def __init__(self, blackNormalPawn = 12, blackRoyalPawn = 0, blackScore = 0, 
+            whiteNormalPawn = 12, whiteRoyalPawn = 0, whiteScore = 0):
+        self.m_BlackNormalPawn = blackNormalPawn
+        self.m_BlackRoyalPawn = blackRoyalPawn
+        self.m_WhiteNormalPawn = whiteNormalPawn
+        self.m_WhiteRoyalPawn = whiteRoyalPawn
+        self.m_BlackScore = blackScore
+        self.m_WhiteScore = whiteScore
 
-    def number_pawn(self, pawnColor : PawnColor):
-        if pawnColor == PawnColor.BLACK:
+    def number_pawn(self, pawnColor : pw.PawnColor):
+        if pawnColor == pw.PawnColor.BLACK:
             return self.m_BlackNormalPawn + self.m_BlackRoyalPawn
         else:
             return self.m_WhiteNormalPawn + self.m_WhiteRoyalPawn
 
-    def add_royal_pawn(self, pawnColor : PawnColor):
-        if pawnColor == PawnColor.BLACK:
+    def add_royal_pawn(self, pawnColor : pw.PawnColor):
+        if pawnColor == pw.PawnColor.BLACK:
             self.m_BlackRoyalPawn += 1
             self.m_BlackNormalPawn -= 1
         else:
             self.m_WhiteRoyalPawn += 1
             self.m_WhiteNormalPawn -= 1
 
-    def add_score(self, pawnColor : PawnColor, points):
-        if pawnColor == PawnColor.BLACK:
+    def add_score(self, pawnColor : pw.PawnColor, points):
+        if pawnColor == pw.PawnColor.BLACK:
             self.m_BlackScore += points
         else:
             self.m_WhiteScore += points
     
-    def remove_pawn(self, pawnColor : PawnColor, pawnType : PawnType):
-        if pawnColor == PawnColor.BLACK:
-            if pawnType == PawnType.ROYAL:
+    def remove_pawn(self, pawnColor : pw.PawnColor, pawnType : pw.PawnType):
+        if pawnColor == pw.PawnColor.BLACK:
+            if pawnType == pw.PawnType.ROYAL:
                 self.m_BlackRoyalPawn -= 1
             else:
                 self.m_BlackNormalPawn -= 1
         else:
-            if pawnType == PawnType.ROYAL:
+            if pawnType == pw.PawnType.ROYAL:
                 self.m_WhiteRoyalPawn -= 1
             else:
                 self.m_WhiteNormalPawn -= 1
@@ -76,14 +85,18 @@ class GameMapState:
             ]
         }
 
+def gameMapState_from_json(jsoned) -> GameMapState:
+    return GameMapState(jsoned['black'][0], jsoned['black'][1], jsoned['black'][2],
+            jsoned['white'][0], jsoned['white'][1], jsoned['white'][2])
+
 gameMapState = GameMapState()
 
 def init():
     for k in range(1, 7):
-        gameMap[1, k] = Pawn(PawnColor.BLACK, PawnType.HORIZONTAL)
-        gameMap[2, k] = Pawn(PawnColor.BLACK, PawnType.DIAGONAL)
+        gameMap[1, k] = pw.Pawn(pw.PawnColor.BLACK, pw.PawnType.HORIZONTAL)
+        gameMap[2, k] = pw.Pawn(pw.PawnColor.BLACK, pw.PawnType.DIAGONAL)
 
-        gameMap[5, k] = Pawn(PawnColor.WHITE, PawnType.DIAGONAL)
-        gameMap[6, k] = Pawn(PawnColor.WHITE, PawnType.HORIZONTAL)
+        gameMap[5, k] = pw.Pawn(pw.PawnColor.WHITE, pw.PawnType.DIAGONAL)
+        gameMap[6, k] = pw.Pawn(pw.PawnColor.WHITE, pw.PawnType.HORIZONTAL)
     
     gameMapState.reset()
